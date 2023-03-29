8f185fc7ad8477ebc9c74c855a08152ff4731be5
Nil Valero <nvalero13@gmail.com>
2023-03-29 22:17:03 +0200

    add thank you component and js

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const submit = (value) => {
-  console.log(value)
+let rating = 0
+
+const submit = () => {
+  if (!rating) {
+    alert('No rating selected')
+  } else {
+    document.querySelector('.rating').classList.add('hidden')
+    document.querySelector('.thank-you').classList.remove('hidden')
+    document.querySelector(
+      '.selected-msg'
+    ).innerHTML = `You selected ${rating} out of 5`
+  }
 }
 
+const select = (e) => {
+  rating = e.target.getAttribute('data')
+
+  if (
+    Object.values(numberButtons).some((el) => el.classList.contains('active'))
+  ) {
+    document.querySelector('.active').classList.remove('active')
+  }
+  e.target.classList.add('active')
+}
+
+const numberButtons = document.querySelectorAll('.numbers button')
+console.log(numberButtons)
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', (e) => select(e))
+})
+
+//
 document.querySelector('.submit').addEventListener('click', () => submit(2))
